1940ce892fb2d396ada5717d67039e51f88c17e5
SabreFoxx <geraldnebs@gmail.com>
2021-05-14 12:30:36 +0100

    ...

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
+  selector: 'nav[app-nav]',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, AfterViewInit {
+  @ViewChild('dropdown') dropdown: ElementRef;
+  isMenuShown: boolean = false;
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  toggleMenu() {
+    this.isMenuShown = !this.isMenuShown;
+  }
 
   ngOnInit(): void {
   }
