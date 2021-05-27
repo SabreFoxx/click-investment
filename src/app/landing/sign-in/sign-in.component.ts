89da1cf234d241d65d2a99462104136e38fa7624
SabreFoxx <geraldnebs@gmail.com>
2021-05-27 13:32:11 +0100

    ...

--- src/app/landing/sign-in/sign-in.component.ts
+++ src/app/landing/sign-in/sign-in.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent implements OnInit, AfterViewInit {
+  @ViewChildren('input') private inputBoxes: QueryList<ElementRef>;
+  @ViewChildren('label') private labels: QueryList<ElementRef>;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.inputBoxes.forEach(box => {
+      let boxLabel = this.labels.find(item => {
+        return item.nativeElement.htmlFor == box.nativeElement.id
+      })
+      box.nativeElement.addEventListener('focus', () => {
+        boxLabel.nativeElement.classList.add('move-label');
+      });
+      box.nativeElement.addEventListener('blur', () => {
+        boxLabel.nativeElement.classList.remove('move-label');
+      });
+    })
+  }
 }
