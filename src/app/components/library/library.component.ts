719919efe0939b3bd9bfdfbfe213dbd26c8256ac
SabreFoxx <geraldnebs@gmail.com>
2021-09-24 11:38:56 +0100

    NgScrollbarModule test

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
-  styleUrls: ['./library.component.scss']
+  styleUrls: ['./library.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LibraryComponent implements OnInit {
 
