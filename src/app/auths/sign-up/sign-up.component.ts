f0524eff958689b8a27bb16d86ab794a12ed2e85
SabreFoxx <geraldnebs@gmail.com>
2021-11-17 23:16:27 +0100

    validator feedback

--- src/app/auths/sign-up/sign-up.component.ts
+++ src/app/auths/sign-up/sign-up.component.ts
@@ -10,24 +10,43 @@ import { passwordValidation } from 'src/miscellaneous/validators';
 export class SignUpComponent implements OnInit, AfterViewInit {
   @ViewChildren('input') private inputBoxes: QueryList<ElementRef>;
   @ViewChildren('label') private labels: QueryList<ElementRef>;
+
   form: FormGroup;
+  firstName: AbstractControl;
+  surname: AbstractControl;
   email: AbstractControl;
+  phone: AbstractControl;
+  password: AbstractControl;
+  retypePassword: AbstractControl;
 
   constructor(fb: FormBuilder) {
     this.form = fb.group({
       'firstName': ['', Validators.required],
       'surname': ['', Validators.required],
-      'email': ['', Validators.email],
-      'password': ['', Validators.compose([Validators.minLength(6)])],
-      'retypePassword': ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+      'email': ['', Validators.compose([Validators.required, Validators.email])],
+      'phone': ['', Validators.minLength(5)],
+      'password': ['', Validators.compose([Validators.required])],
+      'retypePassword': ['', Validators.required]
     });
 
+    this.firstName = this.form.controls['firstName'];
+    this.surname = this.form.controls['surname'];
     this.email = this.form.controls['email'];
+    this.phone = this.form.controls['phone'];
+    this.password = this.form.controls['password'];
+    this.retypePassword = this.form.controls['retypePassword'];
   }
 
   ngOnInit(): void {
-    this.email.valueChanges.subscribe(value => {
-      console.log('email changed to ', value);
+    // every time password is retyped, check that it matches; notify if not
+    this.retypePassword.valueChanges.subscribe(value => {
+      setTimeout(() => {
+        if (this.password.value != this.retypePassword.value)
+          this.retypePassword.setErrors({ "__": "Passwords do not match" });
+      }, 1000);
+      if (this.retypePassword.value != ''
+        && (this.password.value == this.retypePassword.value))
+        this.retypePassword.setErrors(null);
     })
   }
 
@@ -43,5 +62,9 @@ export class SignUpComponent implements OnInit, AfterViewInit {
         boxLabel.nativeElement.classList.remove('move-label');
       });
     })
+  }
+
+  submit(): void {
+    alert('submitted')
   }
 }
--- src/miscellaneous/validators.ts
+++ src/miscellaneous/validators.ts
@@ -2,5 +2,5 @@ import { FormControl } from "@angular/forms";
 
 export function passwordValidation(control: FormControl): { [s: string]: boolean } {
     return (control.value.match(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}$/))
-        ? { "required": true } : null;
+        ? { "incorrectFormat": true } : null;
 }
