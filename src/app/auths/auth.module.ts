951fbcc9ed6a98550498884fab3d16bcb67423a0
SabreFoxx <geraldnebs@gmail.com>
2021-11-17 13:56:45 +0100

    validators

--- src/app/auths/auth.module.ts
+++ src/app/auths/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PreSignupComponent } from './pre-signup/pre-signup.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -17,7 +18,8 @@ export const authRoutes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auths/sign-up/sign-up.component.ts
+++ src/app/auths/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { passwordValidation } from 'src/miscellaneous/validators';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,10 +10,25 @@ import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren }
 export class SignUpComponent implements OnInit, AfterViewInit {
   @ViewChildren('input') private inputBoxes: QueryList<ElementRef>;
   @ViewChildren('label') private labels: QueryList<ElementRef>;
+  form: FormGroup;
+  email: AbstractControl;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      'firstName': ['', Validators.required],
+      'surname': ['', Validators.required],
+      'email': ['', Validators.email],
+      'password': ['', Validators.compose([Validators.minLength(6)])],
+      'retypePassword': ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+
+    this.email = this.form.controls['email'];
+  }
 
   ngOnInit(): void {
+    this.email.valueChanges.subscribe(value => {
+      console.log('email changed to ', value);
+    })
   }
 
   ngAfterViewInit(): void {
