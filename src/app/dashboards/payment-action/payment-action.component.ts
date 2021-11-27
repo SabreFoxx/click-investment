ad2df368bf4b783e747b3d7819e47d27a4547dff
SabreFoxx <geraldnebs@gmail.com>
2021-11-27 19:56:55 +0100

    added payment action component

---
+++ src/app/dashboards/payment-action/payment-action.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-action',
+  templateUrl: './payment-action.component.html',
+  styleUrls: ['./payment-action.component.scss']
+})
+export class PaymentActionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
