fff5d146ce806453bc3c7e11fecfa07714d3638b
SabreFoxx <geraldnebs@gmail.com>
2021-12-13 08:32:24 +0100

    consistency cleanup

--- src/app/pages/deposit/deposit.component.ts
+++ src/app/pages/deposit/deposit.component.ts
@@ -9,12 +9,12 @@ import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/co
 export class DepositComponent implements OnInit {
   @ViewChild('addr') addr: ElementRef;
 
-  constructor(private renderer: Renderer2, private ui: UIAdjustmentService) { }
-
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2, private ui: UIAdjustmentService) {
     this.ui.setBreadcrumbs([{ url: '/app/payments', title: 'Payments' },
     { url: '/app/payments/deposit', title: 'Deposit', forceActive: true }]);
   }
+
+  ngOnInit(): void { }
 
   deposit() {
     this.renderer.setStyle(this.addr.nativeElement, 'opacity', '1');
