157eb5b4bad3477619244aef678bea9c9a5a8f2e
SabreFoxx <geraldnebs@gmail.com>
2021-12-13 08:27:00 +0100

    <input-box> now has readonly

--- src/app/components/input-box/input-box.component.ts
+++ src/app/components/input-box/input-box.component.ts
@@ -19,26 +19,32 @@ export class InputBoxComponent implements AfterViewInit, OnDestroy {
   @Input() errorMsg?: string;
   @Input() type: string = 'text';
   @Input() validationFailed = false;
-  // funny enough, I cannot use formControl as the input's name so I use control
-  @Input() control?: FormControl = null;
+  @Input() readOnly?: boolean = false;
+  @Input() control?: FormControl = null; // Couldn't use formControl as Input name
   @ViewChild('input') private inputBox: ElementRef;
   @ViewChild('label') private boxLabel: ElementRef;
   destroyListeners = new Array(2);
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2) {
+    this.placeholder = '';
+  }
 
   ngAfterViewInit(): void {
     this.destroyListeners[0] = this.renderer.listen(this.inputBox.nativeElement, 'focus',
       event => {
-        this.renderer.addClass(this.boxLabel.nativeElement, 'move-label');
-        // $text-color: #5a5a5a; in variables.scss
-        this.renderer.setStyle(this.boxLabel.nativeElement, 'color', '#5a5a5a');
+        if (!this.readOnly) {
+          this.renderer.addClass(this.boxLabel.nativeElement, 'move-label');
+          // $text-color: #5a5a5a; in variables.scss
+          this.renderer.setStyle(this.boxLabel.nativeElement, 'color', '#5a5a5a');
+        }
       });
     this.destroyListeners[1] = this.renderer.listen(this.inputBox.nativeElement, 'blur',
       event => {
-        this.renderer.removeClass(this.boxLabel.nativeElement, 'move-label');
-        if (<HTMLInputElement>(this.inputBox.nativeElement).value.length)
-          this.renderer.setStyle(this.boxLabel.nativeElement, 'color', 'transparent');
+        if (!this.readOnly) {
+          this.renderer.removeClass(this.boxLabel.nativeElement, 'move-label');
+          if (<HTMLInputElement>(this.inputBox.nativeElement).value.length)
+            this.renderer.setStyle(this.boxLabel.nativeElement, 'color', 'transparent');
+        }
       });
   }
 
