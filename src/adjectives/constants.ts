a3b8efeee47609302bf0244c3f805e00c9230be1
SabreFoxx <geraldnebs@gmail.com>
2022-01-12 15:45:25 +0100

    backend uses https

--- src/adjectives/constants.ts
+++ src/adjectives/constants.ts
@@ -29,7 +29,7 @@ export enum ApiEndpoints {
 }
 
 export const apiPrefix: { prod: string, dev: string, test: string } = {
-    prod: 'http://13.37.162.149:3000/click-inv',
+    prod: 'https://13.37.162.149:3000/click-inv',
     dev: 'http://localhost:3000/click-inv',
     test: 'https://'
 }
